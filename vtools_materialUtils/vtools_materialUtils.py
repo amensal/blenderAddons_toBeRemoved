d768094b6d09f9d87f2aea2862b2ad185bfc7a4a
Antonio <ams.vertigo@gmail.com>
2023-04-19 10:26:24 +0200

    añadido material utils

---
+++ vtools_materialUtils/vtools_materialUtils.py
@@ -0,0 +1,35 @@
+import bpy
+
+
+
+class VTOOLS_OP_removeAllMaterials(bpy.types.Operator):
+    bl_idname = "vtools.remveallmaterials"
+    bl_label = "Remove All"
+    bl_description = "Remove all materials from every object selected"
+    bl_options = {'REGISTER', 'PRESET', 'UNDO'}
+    
+    def execute(self,context):
+        
+        for obj in bpy.context.selected_objects:
+            if obj.type == "MESH":
+                if len(obj.data.materials) > 0:
+                    obj.data.materials.clear()
+    
+        return {'FINISHED'}   
+
+def menu_draw(self, context):
+    self.layout.operator(VTOOLS_OP_removeAllMaterials.bl_idname, text=VTOOLS_OP_removeAllMaterials.bl_label, icon='REMOVE')
+    
+def register():
+    bpy.utils.register_class(VTOOLS_OP_removeAllMaterials)
+    bpy.types.MATERIAL_MT_context_menu.append(menu_draw)
+    
+    #addShortcut()
+   
+def unregister():
+    bpy.utils.unregister_class(VTOOLS_OP_removeAllMaterials)
+    bpy.types.TOPBAR_MT_file.remove(menu_draw)
+    
+    
+if __name__ == "__main__":
+    register()
